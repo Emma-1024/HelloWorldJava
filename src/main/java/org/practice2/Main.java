b0a94255131a73c4d3a954d3a34336c30ce0afb1
Emma <yongjie.wu@outlook.com>
2023-02-16 11:00:50 +0800

    enum

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,20 +1,34 @@
 package org.practice2;
-import org.practice2.example.Adder;
-import org.practice2.example.Util;
+
+import java.beans.*;
+import org.practice2.example.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Adder adder = new Adder();
-        adder.add(2).add(3).inc().add(5);
-        System.out.println(adder.value());
+    public static void main(String[] args) throws IntrospectionException {
+//        Adder adder = new Adder();
+//        adder.add(2).add(3).inc().add(5);
+//        System.out.println(adder.value());
 
-        String[] fields = { "name", "position", "salary" };
-        String table = "employee";
-        String insert = Util.buildInsertSql(table, fields);
-        System.out.println(insert);
-        String s = "INSERT INTO employee (name, position, salary) VALUES (?, ?, ?)";
-        System.out.println(s.equals(insert) ? "测试成功" : "测试失败");
+//        String[] fields = { "name", "position", "salary" };
+//        String table = "employee";
+//        String insert = Util.buildInsertSql(table, fields);
+//        String select = Util.buildSelectSql(table, fields);
+//        System.out.println(insert);
+//        System.out.println(select);
+//        String s = "INSERT INTO employee (name, position, salary) VALUES (?, ?, ?)";
+//        System.out.println(s.equals(insert) ? "测试成功" : "测试失败");
+        Weekday day = Weekday.SUN;
+        System.out.println(day);
+        if (day.dayValue == 6 || day.dayValue == 0) {
+            System.out.println("Today is " + day + ". Work at home!");
+        } else {
+            System.out.println("Today is " + day + ". Work at office!");
+        }
+
     }
 
 
 }
+
+
+
--- src/main/java/org/practice2/example/Util.java
+++ src/main/java/org/practice2/example/Util.java
@@ -1,13 +1,26 @@
 package org.practice2.example;
 
+import java.util.StringJoiner;
+
 public class Util {
-    public static String buildInsertSql(String table, String ... fields){
+    public static String buildInsertSql(String table, String... fields) {
         StringBuilder str = new StringBuilder(1024);
         str.append("INSERT INTO ").append(table).append(" (");
-        for(int i=0;i<fields.length;i++){
-            str.append(fields[i]).append(i==fields.length-1?"":", ");
+        for (int i = 0; i < fields.length; i++) {
+            str.append(fields[i]).append(i == fields.length - 1 ? "" : ", ");
         }
         str.append(") VALUES (?, ?, ?)");
         return str.toString();
+    }
+
+    public static String buildSelectSql(String table, String... fields) {
+        //"SELECT name, position, salary FROM employee".
+//        method 1
+//        var sj = new StringJoiner(", ", "SELECT "," FROM " + table);
+//        for(String field: fields){
+//            sj.add(field);
+//        }
+//        method 2
+        return String.format("SELECT %s FROM %s", String.join(", ", fields), table);
     }
 }
