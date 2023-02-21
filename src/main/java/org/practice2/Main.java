a70a0d5f73f2ce3c5197e996be6e943708d9c41d
Emma <yongjie.wu@outlook.com>
2023-02-21 18:48:07 +0800

    annotation

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,43 +1,45 @@
 package org.practice2;
 
-import java.beans.*;
-import java.math.BigInteger;
-import java.util.Random;
-
-import org.practice2.example.*;
-
-import javax.security.auth.login.LoginException;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String token = login("adminUser", "pass");
-        System.out.println("Token: " + token);
-    }
-
-    static String login(String username, String password) {
-        if (username.equals("admin")) {
-            if (password.equals("password")) {
-                return "succeed";
-            } else {
-                throw new LoginFailedException("incorrect username or password");
+        Person p1 = new Person("Alex", "Shanghai", 34);
+        Person p2 = new Person("", "Shanghai", 20);
+        Person p3 = new Person("Alice", "Shanghai", 199);
+        for (Person p : new Person[]{p1, p2, p3}) {
+            try {
+                check(p);
+                System.out.println("person:" + p + "checked ok.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("person:" + p + "checked failed" + e);
             }
-        } else {
-            throw new UserNotFoundException("User not found");
         }
 
     }
 
-    static class LoginFailedException extends BaseException {
-        public LoginFailedException(String message) {
-            super(message);
-        }
-    }
-
-    static class UserNotFoundException extends BaseException {
-        public UserNotFoundException(String message) {
-            super(message);
+    //  字段长度和数字范围满足@Range的参数定义
+    static void check(Person person) throws IllegalAccessException {
+        for (Field field : person.getClass().getFields()) {
+            Range range = field.getAnnotation(Range.class);
+            if (range != null) {
+                Object value = field.get(person);
+                if (value instanceof String) {
+                    String s = (String) value;
+                    if (s.length() < range.min() || s.length() > range.max()) {
+                        throw new IllegalArgumentException("Invalid field:" + field.getName());
+                    }
+                } else if (value instanceof Integer) {
+                    Integer i = (Integer) value;
+                    if (i < range.min() || i > range.max()) {
+                        throw new IllegalArgumentException("Invalid field:" + field.getName());
+                    }
+                }
+            }
         }
     }
 
 }
+
 
