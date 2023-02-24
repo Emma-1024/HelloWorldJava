9f8933ea41919949007ebace5255161d529453b6
Emma <yongjie.wu@outlook.com>
2023-02-24 13:00:36 +0800

    find student score through map cache

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -2,50 +2,68 @@ package org.practice2;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // 构造从start到end的序列：
-        final int start = 10;
-        final int end = 20;
-        List<Integer> list = new ArrayList<>();
-        for (int i = start; i <= end; i++) {
-            list.add(i);
-        }
-        Collections.shuffle(list);
-        // 随机删除List中的一个元素:
-        Integer removed = list.remove((int) (Math.random() * list.size()));
-        System.out.println(list.toString());
-        int found = findMissingNumber(start, end, list);
-        System.out.println("missing number: " + found);
-        System.out.println(removed == found ? "测试成功" : "测试失败");
+        List<Student> list = List.of(
+                new Student("Bob", 78),
+                new Student("Alice", 85),
+                new Student("Brush", 66),
+                new Student("Newton", 99));
+        var holder = new Students(list);
+        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
+    }
+}
 
+class Students {
+    List<Student> list;
+    Map<String, Integer> cache;
+
+    Students(List<Student> list) {
+        this.list = list;
+        cache = new HashMap<>();
     }
 
-    static int findMissingNumber(int start, int end, List<Integer> list) {
-        int res = 0;
-        //         有序
-//        for(int i: list){
-//            if(i!=start++){
-//                res = --start;
-//                break;
-//            }
-//        }
-        // 无序
-        for (int i = start; i <= end; i++) {
-            if (!list.contains(i)) {
-                res = i;
-                break;
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    int getScore(String name) {
+        // 先在Map中查找:
+        Integer score = this.cache.get(name);
+        if (score == null) {
+            score = findInList(name);
+            if (score != null) {
+                this.cache.put(name, score);
+            } else {
+                System.out.printf("The student's name: %s,%s doesn't exist in the system.%n", name, name);
             }
         }
-        return res;
+        return score == null ? -1 : score;
     }
 
+    Integer findInList(String name) {
+        for (Student s : this.list) {
+            if (s.name.equals(name)) {
+                return s.score;
+            }
+        }
+        return null;
+    }
 }
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
 
 
 
