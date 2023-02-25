6ad1735ca5ba3fb81953a888671c93bca90baac4
Emma <yongjie.wu@outlook.com>
2023-02-25 14:20:45 +0800

    Use TreeMap

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -2,67 +2,42 @@ package org.practice2;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.time.DayOfWeek;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<Student> list = List.of(
-                new Student("Bob", 78),
-                new Student("Alice", 85),
-                new Student("Brush", 66),
-                new Student("Newton", 99));
-        var holder = new Students(list);
-        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
-        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
-        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
-    }
-}
-
-class Students {
-    List<Student> list;
-    Map<String, Integer> cache;
-
-    Students(List<Student> list) {
-        this.list = list;
-        cache = new HashMap<>();
-    }
-
-    /**
-     * 根据name查找score，找到返回score，未找到返回-1
-     */
-    int getScore(String name) {
-        // 先在Map中查找:
-        Integer score = this.cache.get(name);
-        if (score == null) {
-            score = findInList(name);
-            if (score != null) {
-                this.cache.put(name, score);
-            } else {
-                System.out.printf("The student's name: %s,%s doesn't exist in the system.%n", name, name);
+        Map<Student, Integer> map = new TreeMap<>(new Comparator<Student>() {
+            public int compare(Student p1, Student p2) {
+                return Integer.compare(p2.score, p1.score);
             }
+        });
+        map.put(new Student("Tom", 77), 1);
+        map.put(new Student("Bob", 66), 2);
+        map.put(new Student("Lily", 99), 3);
+        for (Student key : map.keySet()) {
+            System.out.println(key);
         }
-        return score == null ? -1 : score;
+        System.out.println(map.get(new Student("Bob", 66))); // null?
     }
 
-    Integer findInList(String name) {
-        for (Student s : this.list) {
-            if (s.name.equals(name)) {
-                return s.score;
-            }
-        }
-        return null;
-    }
 }
 
 class Student {
-    String name;
-    int score;
+    public String name;
+    public int score;
 
     Student(String name, int score) {
         this.name = name;
         this.score = score;
     }
+
+    public String toString() {
+        return String.format("{%s: score=%d}", name, score);
+    }
 }
+
+
 
 
 
