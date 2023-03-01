b9de63ae3d1df0a549b8ed6b638056727ac4352a
Emma <yongjie.wu@outlook.com>
2023-03-01 14:27:28 +0800

    Use Iterator

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,53 +1,48 @@
 package org.practice2;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String hex = toHex(12500);
-        if (hex.equalsIgnoreCase("30D4")) {
-            System.out.println("测试通过");
-        } else {
-            System.out.println("测试失败");
+        ReverseList<String> rlist = new ReverseList<>();
+        rlist.add("Apple");
+        rlist.add("Orange");
+        rlist.add("Pear");
+        for (String s : rlist) {
+            System.out.println(s);
         }
     }
+}
+class ReverseList<T> implements Iterable<T> {
+    private final List<T> list = new LinkedList<>();
 
-    private static String toHex(int i) {
-        Deque<String> deque = new LinkedList<>();
-        String s = Integer.toString(i);
-        int hexBase = 16;
-        while (i > hexBase || i % hexBase != 0) {
-            int remainder = i % hexBase;
-            if(remainder<10){
-              deque.push(String.valueOf(remainder));
-            }
-            else if(remainder==10){
-                deque.push("A");
-            }
-            else if(remainder==11){
-                deque.push("B");
-            }
-            else if(remainder==12){
-                deque.push("C");
-            }
-            else if(remainder==13){
-                deque.push("D");
-            }
-            else if(remainder==14){
-                deque.push("E");
-            }
-            else if(remainder==15){
-                deque.push("F");
-            }
-//            deque.push(Integer.toHexString(i % hexBase));
-            i = i / hexBase;
+    public void add(T t){
+        list.add(t);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ReverseIterator(list.size());
+    }
+
+    class ReverseIterator implements Iterator<T> {
+        int index;
+
+        ReverseIterator(int index) {
+            this.index = index;
         }
-        StringBuilder res = new StringBuilder();
-        int len = deque.size();
-        for (int k = 0; k < len; k++) {
-            res.append(deque.pop());
+
+        @Override
+        public boolean hasNext() {
+            return index > 0;
         }
-        return res.toString();
+
+        @Override
+        public T next() {
+            index--;
+            return ReverseList.this.list.get(index);
+        }
     }
 
 }
