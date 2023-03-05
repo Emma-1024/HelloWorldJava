1dbf06031c9833cbdd609ec52d6a18f8f5f6599f
Emma <yongjie.wu@outlook.com>
2023-03-05 23:19:00 +0800

    deadlock example

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,47 +1,54 @@
 package org.practice2;
 
 public class Main {
+    static final Object LOCK_A = new Object();
+    static final Object LOCK_B = new Object();
     public static void main(String[] args) throws Exception {
-        var c1 = new Counter();
-
-        new Thread(() -> {
-            c1.add(2);
-        }).start();
-        new Thread(() -> {
-            c1.decrease(2);
-        }).start();
+        new Thread1().start();
+        new Thread2().start();
+    }
+    static void sleep1s() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
-class Counter {
-    private int value = 0;
-    private int another = 0;
-    public static final Object lockA = new Object();
-    public static final Object lockB = new Object();
+class Thread1 extends Thread {
 
-    public void add(int m) {
-        synchronized (lockA) {
-            this.value += m;
-            System.out.println("add value" + this.value);
-            synchronized (lockB) {
-                this.another += m;
-                System.out.println("add another" + this.another);
+    public void run() {
+        System.out.println("Thread-1: try get lock A...");
+        synchronized (Main.LOCK_A) {
+            System.out.println("Thread-1: lock A got.");
+            Main.sleep1s();
+            System.out.println("Thread-1: try get lock B...");
+            synchronized (Main.LOCK_B) {
+                System.out.println("Thread-1: lock B got.");
+                Main.sleep1s();
             }
+            System.out.println("Thread-1: lock B released.");
         }
-        System.out.println("add");
+        System.out.println("Thread-1: lock A released.");
     }
+}
 
-    public void decrease(int m) {
-        synchronized (lockA) {
-            this.value -= m;
-            System.out.println("dec value" + this.value);
-            synchronized (lockB) {
-                this.another -= m;
-                System.out.println("dec another" + this.another);
+class Thread2 extends Thread {
+
+    public void run() {
+        System.out.println("Thread-2: try get lock A...");
+        synchronized (Main.LOCK_A) {
+            System.out.println("Thread-2: lock A got.");
+            Main.sleep1s();
+            System.out.println("Thread-2: try get lock B...");
+            synchronized (Main.LOCK_B) {
+                System.out.println("Thread-2: lock B got.");
+                Main.sleep1s();
             }
+            System.out.println("Thread-2: lock B released.");
         }
-        System.out.println("decrease");
+        System.out.println("Thread-2: lock A released.");
     }
-
 }
 
