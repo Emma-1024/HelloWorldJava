5da92a485ed0b544044002e9c0c41790cc4abc3c
Emma <yongjie.wu@outlook.com>
2023-03-15 00:31:22 +0800

    Use Thread CompletableFuture

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,14 +1,13 @@
 package org.practice2;
 
-import org.practice2.example.ThreadFuture;
+import org.practice2.example.ThreadCompletableFuture;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 //        ThreadCondition.threadCondition();
 //        ThreadReadWriteLock.threadReadWriteLock();
 //        ThreadPool.ThreadPool();
-        ThreadFuture.ThreadFuture();
-
+//        ThreadFuture.ThreadFuture();
+        ThreadCompletableFuture.ThreadCompletableFuture();
     }
 }
-
