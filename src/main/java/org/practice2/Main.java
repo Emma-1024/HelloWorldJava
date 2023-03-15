8592e91aae2a7ce68eeb85fb7be259ad26946b54
Emma <yongjie.wu@outlook.com>
2023-03-15 13:02:42 +0800

    Use Thread ForkJoin

--- src/main/java/org/practice2/Main.java
+++ src/main/java/org/practice2/Main.java
@@ -1,6 +1,7 @@
 package org.practice2;
 
 import org.practice2.example.ThreadCompletableFuture;
+import org.practice2.example.ThreadForkJoin;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,6 +9,7 @@ public class Main {
 //        ThreadReadWriteLock.threadReadWriteLock();
 //        ThreadPool.ThreadPool();
 //        ThreadFuture.ThreadFuture();
-        ThreadCompletableFuture.ThreadCompletableFuture();
+//        ThreadCompletableFuture.ThreadCompletableFuture();
+        ThreadForkJoin.threadForkJoin();
     }
 }
